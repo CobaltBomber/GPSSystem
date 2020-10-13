5a0620aa2506ebaced2c0b9eeccd69b2da058216
Zach Wadhams <33103466+CobaltBomber@users.noreply.github.com>
2020-10-12 18:35:55 -0600

    Added files

---
+++ MyGPS.java
@@ -0,0 +1,28 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class MyGPS {
+
+    public static void main(String[] args) {
+
+        In myIn = new In(args[0]);
+
+        String myArray[] = myIn.readAllLines();
+        //String myData[] = new String[];
+
+        for (int i = 0; i < myArray.length, i++) {
+
+        }
+
+        StdOut.println(Arrays.toString(myArray));
+
+
+
+
+
+
+
+    }
+}
